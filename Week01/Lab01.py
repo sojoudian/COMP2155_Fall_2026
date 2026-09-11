bb24b38e4af329c5db381d7d5e5ba74e3cd0cddb
BartyCrouch <sojodyan@gmail.com>
2026-09-11 13:26:24 -0400

    update the lab01

---
+++ Week01/Lab01.py
@@ -0,0 +1,6 @@
+print('Welcome to Network Automation')
+brand = input("Enter the device brand: ")
+model = input("Enter the device model: ")
+IP = input("Enter the IP address of the device: ")
+
+print(f'We are going to config {brand} device with {model} model and IP address: {IP}')
